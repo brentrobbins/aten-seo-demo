fc6c537f1a68057ab7a21e977c16312729635425
Brent Robbins <brent@atendesigngroup.com>
2018-12-10 17:19:11 -0700

    Initial bootstrap theme setup.

---
+++ web/themes/custom/bootstrap4_aten/js/global.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+	'use strict';
+
+	Drupal.behaviors.bootstrap4_aten = {
+		attach: function(context, settings) {}
+	};
+})(jQuery, Drupal);
